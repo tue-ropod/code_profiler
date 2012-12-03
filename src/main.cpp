c2053d07194ac277838d126f6eeec0b5c96013ef
Tim de Jager <tdejager89@gmail.com>
2012-12-03 00:03:26 +0000

    Added profiler for use in your code
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/dev/tue_utils_dev/code_profiler@11771 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#define PROFILEAPP
+#include "profiling/Timer.h"
+#include "profiling/Profiler.h"
+
+
+Timer t;
+int testProf()
+{/*
+    ThreadProfiler::Start("ProfileThis");
+    int i  = sleep(1);
+    ThreadProfiler::Start("ProfileThis2");
+    sleep(2);
+    ThreadProfiler::Stop("ProfileThis2");
+    ThreadProfiler::Stop("ProfileThis");*/
+    t.start();
+    ThreadProfiler::Start("Bla");
+    ThreadProfiler::Stop("Bla");
+    t.stop();
+    std::cout << "Elapsed: " << t.getElapsedTimeInMilliSec() << std::cout;
+}
+
+void testProf2()
+{
+    boost::thread t1(&testProf);
+    t1.join();
+    boost::thread t2(&testProf);
+    t2.join();
+}
+
+int main(int argc, char** argv)
+{
+    testProf();
+}
