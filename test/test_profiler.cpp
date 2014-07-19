fe4cf3cf4f38c0ee15504d0b98d945ffe4addd24
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-07-19 13:29:01 +0000

    Added scoped timer
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_utils/code_profiler@25523 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- test/test_profiler.cpp
+++ test/test_profiler.cpp
@@ -1,40 +1,42 @@
 #include <tue/profiling/profiler.h>
+#include <tue/profiling/scoped_timer.h>
 
 tue::Profiler profiler;
 
 void a()
 {
-    profiler.startTimer("a");
-    profiler.stopTimer();
+    tue::ScopedTimer t(profiler, "a");
 }
 
 void b()
 {
-    profiler.startTimer("b");
+    tue::ScopedTimer t(profiler, "b");
     a();
-    profiler.stopTimer();
 }
 
 void c()
 {
-    profiler.startTimer("c");
+    tue::ScopedTimer t(profiler, "c");
     b();
     a();
-    profiler.stopTimer();
 }
 
 int main(int argc, char **argv) {
 
     profiler.setName("test");
 
+    profiler.startTimer("total");
+
     for(unsigned int i = 0; i < 1000; ++i)
     {
-        profiler.startTimer("total");
+        tue::ScopedTimer t_iteration(profiler, "iteration");
+
         a();
         b();
         c();
-        profiler.stopTimer();
     }
+
+    profiler.stopTimer();
 
     std::cout << profiler << std::endl;
 
